62a7cc112eab6a0a7f64d6642dd50543616523a7
anzar00 <peerzada.anzar@gmail.com>
2022-11-26 01:45:48 +0530

    ENTJ

---
+++ src/lib/personality/ENTJ.svelte
@@ -0,0 +1,108 @@
+<div class="flex items-center justify-center ">
+    <div class="font-semibold text-center rounded-3xl p-10 max-w-xs">
+        <img
+            class="mb-3 w-32 h-32 rounded-full mx-auto"
+            src="https://static.neris-assets.com/images/personality-types/avatars/entj-commander.png"
+            alt=""
+        />
+        <h1 class="text-lg opacity-50">ENTJ</h1>
+        <h1 class="text-lg opacity-50">THE COMMANDER</h1>
+        <div class="flex items-center gap-4">
+            <div>I</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="25"
+                max="100"
+            />
+            <div>E</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>F</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="78"
+                max="100"
+            />
+            <div>P</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>E</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="32"
+                max="100"
+            />
+            <div>J</div>
+        </div>
+
+        <h3 class="text-sm text-gray-400 mt-2 ">
+            Introversion (I) , Intuition (N), Thinking (T), Perceiving (P)
+        </h3>
+
+        <h1 class="mt-4 text-lg opacity-50">The ENTJ Personality Type</h1>
+        <div class="mt-4">
+            Strategic leaders driven to orchestrate change, ENTJs are. They appreciate creating long-term strategies to realise their vision since they are quick to spot inefficiency and envision fresh alternatives. They are typically articulate and perceptive, and they excel in logical reasoning.
+            <br />
+            ENTJs enjoy providing order to the world around them and are analytical and objective. The ENTJ can identify weaknesses in a system and enjoys the process of finding and implementing a more effective solution. ENTJs are assertive and like to be in charge; they see themselves as managers and leaders who coordinate people and processes to accomplish their objectives.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">What does ENTJ stand for?</h1>
+        <div class="mt-4">
+            One of the sixteen personality types identified by Isabel Myers and Katharine Briggs is known by the initials ENTJ. Extraverted, iNtuitive, Thinking, and Judging are its initials. The personality type ENTJ describes someone who gets their energy from being around other people (Extraverted), who prefers ideas and concepts to facts and details (iNtuitive), who bases their decisions on logic and reason (Thinking), and who prefers to be organised and planned rather than spontaneous and flexible (Judging). Because of their intrinsic desire to lead others, ENTJs are sometimes referred to as Commander personalities.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">ENTJ Values and Motivations</h1>
+        <div class="mt-4">
+            ENTJs are frequently highly driven by professional accomplishment and take pleasure in working hard. They have aspirations of being influential and powerful. Making decisions is what they do for a living. They want to be able to make the decision and initiate action.
+            <br />
+            ENTJs are often direct and determined. They are driven to complete tasks and can occasionally be harsh or critical in the process. They are often outgoing and sociable, yet they occasionally fail to recognise others' subtle emotional cues. They frequently like working with people to achieve a shared objective, but they might not have time to take care of their emotions. They are results-driven and aspire to be effective, knowledgeable, and influential.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">How Others See the ENTJ</h1>
+        <div class="mt-4">
+            Natural leaders, ENTJs frequently take charge no matter the situation. They frequently have a distinct vision for the future and have an instinctive sense of how to steer individuals and systems in that direction. They frequently approach each scenario with the efficiency analyst mindset and aren't afraid to suggest improvements. For the ENTJ, the success of their plans is a given; it's only a matter of time before they can influence the participants to make everything happen.
+            <br />
+            When meeting new people, ENTJs are frequently sociable and seem to have have a notion of how they will fit into their big picture. They are frequently blunt and come out as cocky or even arrogant; they assess people and circumstances swiftly and struggle to be anything but truthful about what they observe. ENTJs are sensitive to power dynamics and gravitate toward influential roles and individuals. They have the typical ambition and are frequently very committed to their work. ENTJs enjoy their work more than any other personality type; some even claim that it is what they do for fun.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">
+            How rare is the ENFJ personality type?
+        </h1>
+        <div class="mt-4">
+            ENTJ is the rarest type among women and one of the least prevalent types overall. ENTJs consist of:
+            <ul>
+                <li>2% of the general population</li>
+                <li>3% of men</li>
+                <li>1% of women</li>
+            </ul>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Famous ENTJs</h1>
+        <div class="mt-4">
+            Famous ENTJs include David Letterman, Douglas MacArthur, Harrison Ford, Mindy Kaling, and Quentin Tarantino. Others include Margaret Thatcher, Kamala Harris, Napoleon Bonaparte, Carl Sagan, General Norman Schwarzkopf, and David Letterman.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Facts about INTPs</h1>
+        <div class="mt-4">
+            Interesting facts about the ENTJ:
+            <uL>
+                <li>On personality trait measures, score as Ambitious, Forceful, Optimistic, Egotistical, Adaptable, and Energetic</li>
+                <li>Least likely of all types to report stress resulting from work or finances</li>
+                <li>More likely than average to suffer cardiac problems</li>
+                <li>Among the least likely of all types to believe in a higher spiritual power</li>
+                <li>Among top 4 types in college GPA</li>
+                <li>Among most likely to stay in college</li>
+                <li>Personal values include Home/Family, Achievement, Creativity, and Learning</li>
+                <li>Overrepresented among MBA students and small business owners</li>
+                <li>One of two types most likely to be satisfied with their work                </li>
+            </uL>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">INTP Hobbies and Interests</h1>
+        <div class="mt-4">
+            Taking on leadership roles in neighbourhood organisations, going to social gatherings or sporting events, and participating in competitive sports are all common pastimes among ENTJs. Because ENTJs tend to be so career-focused, they might not have many interests outside of work or they might engage in hobbies that help them advance their jobs.
+        </div>
+    </div>
+</div>
