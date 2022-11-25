6c5108b046958785a303405089bb774994c77648
yajatvishwak <yajat472001@gmail.com>
2022-11-25 15:28:09 +0530

    mood

--- src/lib/Profile.svelte
+++ src/lib/Profile.svelte
@@ -20,7 +20,7 @@
 </script>
 
 <section
-  class="bg-slate-100 w-screen h-full flex flex-col dark:bg-slate-900 p-10 min-h-screen  mx-auto max-w-lg"
+  class="bg-slate-100 relative w-screen h-full flex flex-col dark:bg-slate-900 p-10 min-h-screen mx-auto max-w-lg"
 >
   <Navbar />
 
@@ -90,6 +90,28 @@
     class="flex mt-4 btn btn-outline gap-3   "
   >
     <h4 class="font-semibold ">Personality Details</h4>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+      />
+    </svg>
+  </button>
+  <button
+    on:click={() => {
+      push("/monthly-mood");
+    }}
+    class="flex mt-4 btn btn-outline gap-3   "
+  >
+    <h4 class="font-semibold ">Check Monthly Mood Boards</h4>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { push } from "svelte-spa-router";
+  import { push, location } from "svelte-spa-router";
 </script>
 
 <div
