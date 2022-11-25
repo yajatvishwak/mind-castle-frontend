3f8e696b75378192118c11b389042fcc4f83139e
anzar00 <peerzada.anzar@gmail.com>
2022-11-26 02:09:17 +0530

    ISTJ

---
+++ src/lib/personality/ISTJ.svelte
@@ -0,0 +1,166 @@
+<div class="flex items-center justify-center ">
+    <div class="font-semibold text-center rounded-3xl p-10 max-w-xs">
+        <img
+            class="mb-3 w-32 h-32 rounded-full mx-auto"
+            src="https://static.neris-assets.com/images/personality-types/avatars/istj-logistician.png"
+            alt=""
+        />
+        <h1 class="text-lg opacity-50">ISTJ</h1>
+        <h1 class="text-lg opacity-50">THE INSPECTOR</h1>
+        <div class="flex items-center gap-4">
+            <div>I</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="25"
+                max="100"
+            />
+            <div>E</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>F</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="78"
+                max="100"
+            />
+            <div>P</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>E</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="32"
+                max="100"
+            />
+            <div>J</div>
+        </div>
+
+        <h3 class="text-sm text-gray-400 mt-2 ">
+            Introversion (I) , Intuition (N), Thinking (T), Perceiving (P)
+        </h3>
+
+        <h1 class="mt-4 text-lg opacity-50">The ISTJ Personality Type</h1>
+        <div class="mt-4">
+            ISTJs are responsible planners who are compelled to establish and
+            uphold order within institutions and processes. They prefer to
+            follow a set of rules for everything they do and are clean and
+            organised both inside and out. ISTJs are dependable and obedient
+            people who value tradition and adhere to rules.
+            <br />
+            ISTJs consistently and effectively contribute. Even though they are introverted,
+            ISTJs are rarely alone; they are curious about how they might contribute
+            to established organisations and systems and are aware of their place
+            in the world. They are concerned with preserving the social structure
+            and ensuring that expectations are realised.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">What does ISTJ stand for?</h1>
+        <div class="mt-4">
+            One of the sixteen personality types developed by Isabel Myers and
+            Katharine Briggs is referred to by the acronym ISTJ. Introverted,
+            Sensing, Thinking, and Judging are its initials. The personality
+            type ISTJ describes someone who gets their energy from alone time
+            (Introverted), who prefers facts and details to thoughts and notions
+            (Sensing), who bases their decisions on logic and reason (Thinking),
+            and who prefers to be ordered and planned rather than spontaneous
+            and flexible (Judging). Because of their attention to detail and
+            desire to carry out tasks precisely, ISTJs are frequently referred
+            to as Inspector personalities.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">ISTJ Values and Motivations</h1>
+        <div class="mt-4">
+            ISTJs value predictability over creativity and like to know the
+            rules of the game. They prefer to be in familiar settings and rely
+            on their past experiences for guidance. ISTJs respect the value of
+            consistent practise to hone their talents and trust the
+            tried-and-true approach.
+            <br />
+            ISTJs are persistent and work diligently until a task is completed. They
+            are meticulous and analytical people who frequently take pleasure in
+            jobs that call for them to employ deductive reasoning to come up with
+            solutions. They pay special attention to the little things and scrutinise
+            everything to ensure accuracy. ISTJs labour methodically to bring order
+            to their own little corners of the world with their clear logic and attention
+            to detail.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">How Others See the ISTJ</h1>
+        <div class="mt-4">
+            The ISTJ personality type exudes a sober, traditional aura. They
+            often seek for predictable environments where they are aware of
+            their function because they want to know the rules of the game and
+            play by them. The ISTJ may be helpful even in social situations
+            (like as arranging jackets and hats at a reception) because they
+            often feel more at ease managing a task than they do mingling with
+            strangers. When assigned a task, they are very dependable and
+            complete it successfully.
+            <br />
+            ISTJs rarely draw attention to themselves and are pragmatic and no-nonsense.
+            They have a fondness for the classics and choose their outfits and possessions
+            more for practicality than for fashion. ISTJs are known for their clear
+            speech and keen attention to detail. They typically show greater enthusiasm
+            while imparting factual information than when delving into ethereal thoughts
+            or untested theories.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">
+            How rare is the ISTJ personality type?
+        </h1>
+        <div class="mt-4">
+            The ISTJ personality type is the most prevalent in men and the third
+            most prevalent personality type overall. ISTJs consist of:
+            <ul>
+                <li>12% of the general population</li>
+                <li>16% of men</li>
+                <li>7% of women</li>
+            </ul>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Famous ISTJs</h1>
+        <div class="mt-4">
+            Famous ISTJs include George H.W. Bush, Queen Victoria, Harry Truman,
+            Warren Buffett, and J.D. Rockefeller.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Facts about ISTJs</h1>
+        <div class="mt-4">
+            Interesting facts about the ISTJ:
+            <uL>
+                <li>
+                    On personality trait measures, score as Calm, Stable,
+                    Steady, Cautious, and Conventional
+                </li>
+                <li>
+                    More likely than other types to experience cardiac problems
+                    and hypertension
+                </li>
+                <li>More likely than other types to experience chronic pain</li>
+                <li>Among four highest types in college GPA</li>
+                <li>More frequent among African Americans</li>
+                <li>Personal values include Financial Security</li>
+                <li>
+                    Most likely of all types to enjoy a work environment where
+                    “everything is done by the book”
+                </li>
+                <li>
+                    Overrepresented among bank officers, financial managers, MBA
+                    students, and small business owners
+                </li>
+                <li>
+                    Often found in careers in management, administration, law
+                    enforcement, and accounting
+                </li>
+            </uL>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">ISTJ Hobbies and Interests</h1>
+        <div class="mt-4">
+            Games of concentration like chess and trivial pursuit are popular
+            pastimes among ISTJs, as are computer games, sports watching,
+            working out, and solitary sports like golf.
+        </div>
+    </div>
+</div>
