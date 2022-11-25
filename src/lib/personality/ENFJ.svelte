0be6d7303c616fd111f1045331bc44c29ca3ddf3
anzar00 <peerzada.anzar@gmail.com>
2022-11-25 21:32:54 +0530

    ENFJ

---
+++ src/lib/personality/ENFJ.svelte
@@ -0,0 +1,177 @@
+<div class="flex items-center justify-center ">
+    <div class="font-semibold text-center rounded-3xl p-10 max-w-xs">
+        <img
+            class="mb-3 w-32 h-32 rounded-full mx-auto"
+            src="https://static.neris-assets.com/images/personality-types/avatars/intp-logician.png"
+            alt=""
+        />
+        <h1 class="text-lg opacity-50">ENFJ</h1>
+        <h1 class="text-lg opacity-50">THE TEACHER</h1>
+        <div class="flex items-center gap-4">
+            <div>I</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="25"
+                max="100"
+            />
+            <div>E</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>F</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="78"
+                max="100"
+            />
+            <div>P</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>E</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="32"
+                max="100"
+            />
+            <div>J</div>
+        </div>
+
+        <h3 class="text-sm text-gray-400 mt-2 ">
+            Introversion (I) , Intuition (N), Thinking (T), Perceiving (P)
+        </h3>
+
+        <h1 class="mt-4 text-lg opacity-50">The ENFJ Personality Type</h1>
+        <div class="mt-4">
+            Idealistic planners, ENFJs are determined to carry out their vision
+            of what is best for humanity. Because of their capacity for
+            recognising potential in others and their charismatic ability to
+            influence others, they frequently serve as catalysts for human
+            growth. They are fervent about the possibilities for people and are
+            values- and vision-driven.
+            <br />
+            ENFJs frequently have a lot on their plates and tend to be enthusiastic
+            and driven people. They have a keen awareness of others' needs and are
+            sensitive to human suffering, but they also have a tendency to be upbeat
+            and forward-looking, instinctively sensing opportunities for progress.
+            While the ENFJ is ambitious, it is not self-serving; rather, they feel
+            personally accountable for improving the world.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">What does ENFJ stand for?</h1>
+        <div class="mt-4">
+            One of the sixteen personality types developed by Isabel Myers and
+            Katharine Briggs is referred to by the acronym ENFJ. Extraverted,
+            iNtuitive, Feeling, and Judging are its initials. Extroverted,
+            iNtuitive, feeling, and extraverted are all characteristics of the
+            ENFJ personality type. They focus on ideas and concepts rather than
+            facts and details, make decisions based on their values and
+            feelings, and prefer to be organised and planned rather than
+            spontaneous and flexible. Because of their desire to foster the
+            growth and development of others, ENFJs are sometimes referred to as
+            teacher personalities.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">ENFJ Values and Motivations</h1>
+        <div class="mt-4">
+            ENFJs are motivated by a strong sense of empathy and altruism. They
+            frequently serve as an emotional barometer for others around them
+            since they have an intuitive awareness of others' emotions. Their
+            compassion, however, is not limited to those close to them; they
+            frequently have a humanitarian outlook and may genuinely care about
+            the woes of the entire human race. They often feel other people's
+            emotions firsthand and are moved to action when they witness
+            suffering.
+            <br />
+            ENFJs value intimate relationships and mutual support and think that
+            working together is the best approach to accomplish goals. They seek
+            approval and are extremely sensitive to both positive and negative criticism.
+            The ENFJ may be frustrated when others are not as sincere in their goals
+            as they are since they want the best not only from themselves but also
+            from others. ENFJs put up a lot of effort to keep their bonds strong
+            and to contribute positively to their families, groups, and communities.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">How Others See the ENFJ</h1>
+        <div class="mt-4">
+            Natural educators, ENFJs are frequently seen getting others to
+            participate in educational activities. They have a propensity to
+            assume control and direct a group of people toward the experiences
+            and activities that will best serve their learning and development.
+            They have a keen sense of human potential, and they inspire others
+            to develop their strengths further by being warm and charismatic.
+            They are usually active and busy, and they frequently exhibit
+            visible vigour when inspiring others to learn new things.
+            <br />
+            ENFJs are often good communicators with a gift for connecting with others
+            through language. They have a keen sense of interpersonal interactions
+            and like discussing them. They frequently take pleasure in assisting
+            others in resolving personal issues and love imparting their knowledge
+            of people, their emotions, and their motivations. They can become worn
+            out if there is too much negative emotion around because they can be
+            overly sympathetic at times.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">
+            How rare is the ENFJ personality type?
+        </h1>
+        <div class="mt-4">
+            ENFJ is one of the less prevalent personality types, particularly
+            among men. ENFJ is the second-rarest type among guys. ENFJs consist
+            of:
+            <ul>
+                <li>3% of the general population</li>
+                <li>3% of women</li>
+                <li>2% of men</li>
+            </ul>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Famous ENFJs</h1>
+        <div class="mt-4">
+            Oprah Winfrey, Martin Luther King, Jr., Margaret Mead, Maya Angelou,
+            Ralph Nader, Abraham Maslow, and Dr. Phil McGraw are notable ENFJs.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Facts about INTPs</h1>
+        <div class="mt-4">
+            Interesting facts about the INTP:
+            <uL>
+                <li>
+                    On personality trait scales, scored as Active, Pleasant,
+                    Sociable, Demanding, Impatient, Appreciative, and
+                    Compromising
+                </li>
+                <li>
+                    Most likely of all types to cope with stress by exercising
+                </li>
+                <li>
+                    Most likely of all types to believe in a higher spiritual
+                    power
+                </li>
+                <li>
+                    Ranked by psychologists as among least likely to have
+                    trouble in school
+                </li>
+                <li>
+                    Personal values include Friendships, Education & Learning,
+                    Creativity, and Community Service
+                </li>
+                <li>
+                    Among types highest in job satisfaction, but also among most
+                    likely to report plans to leave their jobs
+                </li>
+                <li>
+                    Commonly found in careers in religion, teaching, and the
+                    arts
+                </li>
+            </uL>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">INTP Hobbies and Interests</h1>
+        <div class="mt-4">
+            The ENFJ enjoys planning social gatherings, reading, the arts,
+            visiting museums, listening to music, writing, storytelling, and
+            gourmet cooking.
+        </div>
+    </div>
+</div>
