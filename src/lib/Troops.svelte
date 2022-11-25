49ada9884192dfcf7d0cb9ec861e7cd0a9348acd
yajatvishwak <yajat472001@gmail.com>
2022-11-25 15:31:03 +0530

    lol

--- src/lib/Troops.svelte
+++ src/lib/Troops.svelte
@@ -44,7 +44,9 @@
   </div>
 
   {#each data.troops as troop}
-    <button
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div
+      class=""
       on:click={() => {
         push(`/troop/${troop.id}`);
       }}
@@ -52,9 +54,9 @@
       <div
         class="flex rounded-lg flex-col  dark:bg-slate-800  bg-white p-5 shadow-sm my-2"
       >
-        <h4 class="font-semibold   line-clamp-2">
+        <div class="font-semibold   line-clamp-2">
           {troop.troopTitle}
-        </h4>
+        </div>
 
         <div class="flex text-sm gap-2 mt-2 items-center">
           <div class="w-7 rounded-full bg-indigo-400">
@@ -70,6 +72,6 @@
           </div>
         </div>
       </div>
-    </button>
+    </div>
   {/each}
 </section>
