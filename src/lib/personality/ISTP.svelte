da3eeae2ba9286e83c041999d458359bdf8482ab
anzar00 <peerzada.anzar@gmail.com>
2022-11-26 02:04:18 +0530

    ISTP

---
+++ src/lib/personality/ISTP.svelte
@@ -0,0 +1,176 @@
+<div class="flex items-center justify-center ">
+    <div class="font-semibold text-center rounded-3xl p-10 max-w-xs">
+        <img
+            class="mb-3 w-32 h-32 rounded-full mx-auto"
+            src="https://static.neris-assets.com/images/personality-types/avatars/istp-virtuoso.png"
+            alt=""
+        />
+        <h1 class="text-lg opacity-50">ISTP</h1>
+        <h1 class="text-lg opacity-50">THE CRAFTSMAN</h1>
+        <div class="flex items-center gap-4">
+            <div>I</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="25"
+                max="100"
+            />
+            <div>E</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>F</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="78"
+                max="100"
+            />
+            <div>P</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>E</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="32"
+                max="100"
+            />
+            <div>J</div>
+        </div>
+
+        <h3 class="text-sm text-gray-400 mt-2 ">
+            Introversion (I) , Intuition (N), Thinking (T), Perceiving (P)
+        </h3>
+
+        <h1 class="mt-4 text-lg opacity-50">The ISTP Personality Type</h1>
+        <div class="mt-4">
+            ISTPs are keen observers who enjoy problem-solving and have a
+            working knowledge of mechanics. They approach their surroundings
+            with a flexible logic and seek for workable answers to the issues at
+            hand. They are independent and flexible, and they frequently engage
+            with their environment in a self-directed, impulsive way.
+            <br />
+            ISTPs pay close attention to the small details and react quickly to outside
+            stimuli. They can move rapidly and react to emergencies because they
+            have a keen sense of their surroundings. The ISTP appreciates taking
+            initiative and approaches the environment with a great appreciation for
+            the physical and sensory pleasures it has to offer. ISTPs are reserved
+            but not withdrawn.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">What does ISTP stand for?</h1>
+        <div class="mt-4">
+            One of the sixteen personality types identified by Isabel Myers and
+            Katharine Briggs is known by the initials ISTP. Introverted,
+            Sensing, Thinking, and Perceiving are its initials. The personality
+            type ISTP describes someone who enjoys spending time alone
+            (Introverted), who concentrates on facts and details rather than
+            ideas and concepts (Sensing), who makes decisions using logic and
+            reason (Thinking), and who prefers to be flexible and impromptu
+            rather than planned and methodical (Perceiving). Due to their
+            propensity for mechanical aptitude and tool proficiency, ISTPs are
+            commonly referred to as craftsperson personalities.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">ISTP Values and Motivations</h1>
+        <div class="mt-4">
+            ISTPs often have a special aptitude to manipulate the tools in their
+            settings and are naturally fascinated about the mechanics of the
+            world around them. They frequently become experts in the usage and
+            operation of machinery, instruments, and equipment because they have
+            a propensity for studying how things function. They seek
+            understanding, but in a pragmatic sense; they prefer to immediately
+            apply their technical knowledge and grow weary of theory.
+            <br />
+            ISTPs have a propensity for objectivity and favour the simplicity of
+            mechanical things to the intricacy of emotional complexity. Independent
+            and reclusive, ISTPs value their own space and desire the freedom to
+            be impulsive and make decisions on their own. Relationships with ISTPs
+            are carefully chosen, and they value partners who give them plenty of
+            latitude to pursue their own interests.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">How Others See the ISTP</h1>
+        <div class="mt-4">
+            ISTPs are frequently cautious and even distant. The ISTP observes
+            their surroundings carefully, taking in the intricacies and facts
+            while recognising sensory information and how things function. They
+            are tolerant and nonjudgmental. They frequently pay attention to
+            what needs to be done, attending modestly and subtly to the urgent
+            demands of the present. They frequently keep their personal life
+            secret and tend to prefer action to speech. ISTPs are unlikely to
+            "open up" to new people in the traditional sense, but they may
+            connect with others by engaging in similar activities or cooperating
+            to find a solution to a problem in the real world.
+            <br />
+            ISTPs are typically mechanical and adept with their hands. They are frequently
+            drawn to physically demanding pastimes like carpentry or crafts, and
+            you could find them tinkering with home items, bicycles, computers, or
+            automobiles. They frequently possess a natural understanding of machines
+            and a great capacity for problem-solving. ISTPs appreciate danger and
+            action, and they frequently take pleasure in exhilarating hobbies like
+            motorcycling, extreme sports, or using weapons.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">
+            How rare is the ISTP personality type?
+        </h1>
+        <div class="mt-4">
+            Males are significantly more likely than females to have the ISTP
+            personality type. It ranks as the third most prevalent kind in men
+            overall. It is the fourth rarest condition in women. ISTPs consist
+            of:
+            <ul>
+                <li>5% of the general population</li>
+                <li>9% of men</li>
+                <li>2% of women</li>
+            </ul>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Famous ISTPs</h1>
+        <div class="mt-4">
+            Famous ISTPs include Amelia Earhart, Clint Eastwood, Katherine
+            Hepburn, Miles Davis, Tiger Woods, Chuck Yaeger, Lance Armstrong,
+            and Bruce Lee.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Facts about ISTPs</h1>
+        <div class="mt-4">
+            Interesting facts about the ISTP:
+            <uL>
+                <li>
+                    On personality trait measures, score as Critical, Detached,
+                    Guarded, Independent, and Resourceful
+                </li>
+                <li>
+                    Commonly found in populations of male college scholarship
+                    athletes
+                </li>
+                <li>More likely than other types to suffer cardiac problems</li>
+                <li>
+                    Lowest ranked of all types in using social coping resources
+                </li>
+                <li>
+                    One of four types least satisfied with their marriage or
+                    intimate relationship
+                </li>
+                <li>Among types least likely to complete college</li>
+                <li>
+                    Personal values include Autonomy; at work, value Stability,
+                    Security, Independence, and Achievement
+                </li>
+                <li>
+                    Commonly found in skilled trades, technical fields,
+                    agriculture, law enforcement, and military occupations
+                </li>
+            </uL>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">ISTP Hobbies and Interests</h1>
+        <div class="mt-4">
+            For an ISTP, archery, weaponry, hunting, scuba diving, rappelling,
+            aviation, skydiving, motorcycling, and other extreme sports are
+            among the preferred pastimes. They frequently gravitate toward
+            dangerous, exciting, and manual labor-intensive activities.
+        </div>
+    </div>
+</div>
