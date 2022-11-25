abf91b0e060b09a38ff279789e9c7c0ad91c047a
anzar00 <peerzada.anzar@gmail.com>
2022-11-26 02:25:36 +0530

    ISFJ

---
+++ src/lib/personality/ISFJ.svelte
@@ -0,0 +1,180 @@
+<div class="flex items-center justify-center ">
+    <div class="font-semibold text-center rounded-3xl p-10 max-w-xs">
+        <img
+            class="mb-3 w-32 h-32 rounded-full mx-auto"
+            src="https://static.neris-assets.com/images/personality-types/avatars/isfj-defender.png"
+            alt=""
+        />
+        <h1 class="text-lg opacity-50">ISFJ</h1>
+        <h1 class="text-lg opacity-50">THE PROTECTOR</h1>
+        <div class="flex items-center gap-4">
+            <div>I</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="25"
+                max="100"
+            />
+            <div>E</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>F</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="78"
+                max="100"
+            />
+            <div>P</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>E</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="32"
+                max="100"
+            />
+            <div>J</div>
+        </div>
+
+        <h3 class="text-sm text-gray-400 mt-2 ">
+            Introversion (I) , Intuition (N), Thinking (T), Perceiving (P)
+        </h3>
+
+        <h1 class="mt-4 text-lg opacity-50">The ISFJ Personality Type</h1>
+        <div class="mt-4">
+            ISFJs are devoted upholders of customs and institutions who work
+            hard. They are driven to provide for others and safeguard them from
+            the dangers of life because they are realistic, sympathetic, and
+            caring.
+            <br />
+            ISFJs appreciate contributing to society's established systems since
+            they are practical and conventional. They have a strong sense of obligation
+            to others and are dependable, committed workers. Particularly while attending
+            to others' needs, they concentrate on performing their duty. They want
+            other people to recognise their dependability and ability to fulfil obligations.
+            They work diligently and methodically and don't give up until the project
+            is finished.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">What does ISFJ stand for?</h1>
+        <div class="mt-4">
+            One of the sixteen personality types developed by Isabel Myers and
+            Katharine Briggs is referred to by the acronym ISFJ. Introverted,
+            Sensing, Feeling, and Judging are its initials. ISFJs are described
+            as being introverted, sensing, feeling, and sensing with a
+            preference for planning and organisation over spontaneity and
+            flexibility. They also tend to be more focused on facts and details
+            than thoughts and concepts (Judging). Because of their need to
+            ensure that others are secure and taken care of, ISFJs are
+            occasionally referred to as Protector personalities.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">ISFJ Values and Motivations</h1>
+        <div class="mt-4">
+            ISFJs are diligent in their behaviour and motivated by their own
+            particular ideals. They often desire to put forth a lot of effort,
+            get along with people, and fulfil their obligations.
+            <br />
+            Relationships are very important to ISFJs, and they work hard to maintain
+            harmony and cooperation with others. They tend to maintain a strong sense
+            of loyalty to their families and seek consistency and longevity in their
+            relationships. People they know they can depend on in the long run are
+            the ones with whom they feel the most connected.
+            <br />
+            ISFJs value tradition and like learning about how things used to be done.
+            They want to follow the right, conventional course of action and are
+            faithful to established practises and ideals. They place a high value
+            on assimilating into established institutions and making a positive contribution
+            to the upkeep of robust, stable social systems. They frequently play
+            the historian in groups, ensuring that new members respect and honour
+            the old traditions.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">How Others See the ISFJ</h1>
+        <div class="mt-4">
+            ISFJs are often modest and meek, and they don't like to draw
+            attention to themselves. They are frequently observed quietly
+            helping people in a modest manner. They are dependable and diligent,
+            and they frequently dedicate themselves to jobs and endeavours with
+            the intention of assisting their family, friends, and communities.
+            Although they frequently participate in social groups, they like to
+            stay out of the limelight and prefer to be found quietly performing
+            their duties.
+            <br />
+            ISFJs are relationship-oriented, but they might be guarded around strangers.
+            Rarely do they rapidly reveal personal information. They frequently provide
+            specifics from their own personal experience while remaining attentive
+            and aware of their surroundings. They frequently talk about their personal
+            experiences and things they have firsthand knowledge about. They listen
+            with empathy and frequently retain specifics about people. When establishing
+            a connection, they frequently find it interesting to learn things about
+            others.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">
+            How rare is the ISFJ personality type?
+        </h1>
+        <div class="mt-4">
+            The most prevalent type in the US population, particularly among
+            women, is the ISFJ. ISFJs consist of:
+            <ul>
+                <li>14% of the general population</li>
+                <li>19% of women</li>
+                <li>8% of men</li>
+            </ul>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Famous ISFJs</h1>
+        <div class="mt-4">
+            Famous ISFJs include Mother Teresa, Kate Middleton, Rosa Parks,
+            Princess Mary of Denmark, George H.W. Bush, Beyoncé, Laura Bush,
+            King George VI, and Clara Barton.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Facts about ISFJs</h1>
+        <div class="mt-4">
+            Interesting facts about the ISFJ:
+            <uL>
+                <li>
+                    On personality trait measures, score as Conservative,
+                    Conventional, Guarded, and Reserved
+                </li>
+                <li>
+                    Among types most likely to believe in a higher spiritual
+                    power
+                </li>
+                <li>More likely than average to experience chronic pain</li>
+                <li>Among types most likely to suffer heart disease</li>
+                <li>
+                    Second most common type among education majors in college
+                </li>
+                <li>
+                    More likely than other types to watch more than 3 hours of
+                    television per day
+                </li>
+                <li>
+                    Personal values include Happy family, Health and
+                    Spirituality
+                </li>
+                <li>
+                    Overrepresented among MBA students and male small business
+                    owners
+                </li>
+                <li>Among three types with the lowest income</li>
+                <li>
+                    Commonly found in education, health care, and religious
+                    occupations
+                </li>
+            </uL>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">ISFJ Hobbies and Interests</h1>
+        <div class="mt-4">
+            Cooking, gardening, painting, crafts, picnics, nature walks, and
+            watching movies are among the ISFJs' favourite pastimes. They
+            frequently observe their loved ones participating in the hobbies and
+            pastimes they enjoy.
+        </div>
+    </div>
+</div>
