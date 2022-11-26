b418ff8afb400d297879582ce5f9619943e02cb4
anzar00 <peerzada.anzar@gmail.com>
2022-11-26 15:39:36 +0530

    INFJ

---
+++ src/lib/personality/INFJ.svelte
@@ -0,0 +1,115 @@
+<div class="flex items-center justify-center ">
+    <div class="font-semibold text-center rounded-3xl p-10 max-w-xs">
+        <img
+            class="mb-3 w-32 h-32 rounded-full mx-auto"
+            src="https://static.neris-assets.com/images/personality-types/avatars/infj-advocate.png"
+            alt=""
+        />
+        <h1 class="text-lg opacity-50">INFJ</h1>
+        <h1 class="text-lg opacity-50">THE COUNSELOR</h1>
+        <div class="flex items-center gap-4">
+            <div>I</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="25"
+                max="100"
+            />
+            <div>E</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>F</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="78"
+                max="100"
+            />
+            <div>P</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>E</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="32"
+                max="100"
+            />
+            <div>J</div>
+        </div>
+
+        <h3 class="text-sm text-gray-400 mt-2 ">
+            Introversion (I) , Intuition (N), Thinking (T), Perceiving (P)
+        </h3>
+
+        <h1 class="mt-4 text-lg opacity-50">The INFJ Personality Type</h1>
+        <div class="mt-4">
+            INFJs are nurturing creatives who have a strong sense of morality and a desire to help others reach their full potential. They are talented at providing people with unique answers to their individual problems since they are creative and dedicated.
+            <br />
+            The Counselor has a special capacity for perceiving others' feelings and intentions; frequently, they can tell how someone is feeling even before they are aware of it themselves. They have tremendous faith in their ability to read others and trust their perceptions of others. The INFJ is a secretive type who is choosy about communicating inner thoughts and feelings; despite being sensitive, they are also guarded.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">What does INFJ stand for?</h1>
+        <div class="mt-4">
+            The Myers-Briggs Type Indicator (MBTI®), which was developed by Katharine Briggs and Isabel Myers, has sixteen different personality types. The acronym INFJ stands for Introversion, iNtuition, Feeling, and Judging. These four fundamental personality qualities are based on the research of psychologist C.G. Jung.
+            <br />
+            Each of the INFJ personality type's four letters stands for a crucial characteristic. INFJs prefer to be ordered and prepared rather than spontaneous and flexible. They are energised by time alone, focus on ideas and concepts rather than facts and specifics, and make decisions based on their feelings and ideals (Judging).
+            <br />
+            Because of their propensity to be idealistic, empathetic, and sensitive, people with the INFJ personality type are sometimes known as "Counselors." The INFJ also goes by the moniker:
+            <ul>
+                <li>The Insightful Visionary (MBTI)</li>
+                <li>The Advocate (16Personalities)</li>
+            </ul>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">INFJ Values and Motivations</h1>
+        <div class="mt-4">
+            INFJs follow a carefully thought-out set of personal ideals. They have a strong sense of utopianism and can easily picture a better, more ideal future. The hard realities of the present can discourage them, yet they are often driven and persistent in acting positively despite this. The INFJ has a strong internal motivation to do their part to improve the world.
+            <br />
+            INFJs seek a purpose in life and meaningful relationships. They like to have emotional connection with a restricted group of devoted individuals and do not frequently share themselves openly. They deeply cherish genuine connections with individuals they trust, despite the fact that their complex inner lives can give them the impression that they are mysterious or private.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">How Others See the INFJ</h1>
+        <div class="mt-4">
+            INFJs frequently present as reserved, compassionate, and sensitive people who pay close attention to the opinions and concerns of others. They have a keen sense of human nature and desire to aid others in understanding. INFJs are not afraid of having complicated personal issues because they are actually highly complex and have a deep inner life that few others are aware of. They give ethics a lot of thought and have strong emotions. Counselors may surprise others with their fervour when one of their principles is attacked or questioned because they originally come across as so kind and restrained. Their apparent composure masks the intricacy of their inner selves.
+            <br />
+            INFJs might be challenging to get to know since they are such complicated individuals who may be reluctant to interact with someone who might not understand or value them. They are devoted to their own set of ideals and will not follow others down a route that does not feel true to them, even though they want to get along with others and support them in their endeavours. They tend to withdraw when they feel their morals aren't being upheld or when they have a gut feeling that someone's motives aren't genuine.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">
+            How rare is the INFJ personality type?
+        </h1>
+        <div class="mt-4">
+            The INFJ personality type is the least common. It is the third least prevalent kind among women and the least prevalent form among men (after INTJ and ENTJ). INFJs consist of:
+            <ul>
+                <li>2% of the general population</li>
+                <li>2% of women</li>
+                <li>1% of men</li>
+            </ul>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Famous INFJs</h1>
+        <div class="mt-4">
+            In addition to Mohandas Gandhi, Eleanor Roosevelt, Emily Bronte, Jane Goodall, Carl Jung, Fyodor Dostoevsky, Florence Nightingale, Shirley MacLaine, Jimmy Carter, Brené Brown, and Edward Snowden, INFJs have also contributed to a number of notable works.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Facts about INFJs</h1>
+        <div class="mt-4">
+            Interesting facts about the INFJ:
+            <uL>
+                <li>Least common type in the population</li>
+                <li>On personality trait scales, scored as Sincere, Sympathetic, Unassuming, Submissive, Easygoing, Reserved and Patient</li>
+                <li>Among highest of all types in college GPA</li>
+                <li>Among most likely to stay in college</li>
+                <li>Most likely of all types to cope with stress by seeing a therapist</li>
+                <li>Highest of all types in marital dissatisfaction</li>
+                <li>Personal values include Spirituality, Learning, and Community Service</li>
+                <li>Commonly found in careers in religion, counseling, teaching, and the arts</li>
+            </uL>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">INFJ Hobbies and Interests</h1>
+        <div class="mt-4">
+            Writing, appreciating art, attending cultural events, reading, engaging in small-group conversations, and playing or listening to music are among the INFJ's favourite pastimes.
+        </div>
+    </div>
+</div>
