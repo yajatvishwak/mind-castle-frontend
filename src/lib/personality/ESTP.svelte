5ef0fb3da4c8716f14ccf1e50ecdef8cf789b0a7
anzar00 <peerzada.anzar@gmail.com>
2022-11-26 15:54:32 +0530

    ESTP

---
+++ src/lib/personality/ESTP.svelte
@@ -0,0 +1,164 @@
+<div class="flex items-center justify-center ">
+    <div class="font-semibold text-center rounded-3xl p-10 max-w-xs">
+        <img
+            class="mb-3 w-32 h-32 rounded-full mx-auto"
+            src="https://static.neris-assets.com/images/personality-types/avatars/estp-entrepreneur.png"
+            alt=""
+        />
+        <h1 class="text-lg opacity-50">ESTP</h1>
+        <h1 class="text-lg opacity-50">THE DYNAMO</h1>
+        <div class="flex items-center gap-4">
+            <div>I</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="25"
+                max="100"
+            />
+            <div>E</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>F</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="78"
+                max="100"
+            />
+            <div>P</div>
+        </div>
+
+        <div class="flex items-center gap-4">
+            <div>E</div>
+            <divrogress
+                class="progress progress-success w-36"
+                value="32"
+                max="100"
+            />
+            <div>J</div>
+        </div>
+
+        <h3 class="text-sm text-gray-400 mt-2 ">
+            Introversion (I) , Intuition (N), Thinking (T), Perceiving (P)
+        </h3>
+
+        <h1 class="mt-4 text-lg opacity-50">The ESTP Personality Type</h1>
+        <div class="mt-4">
+            ESTPs are enthusiastic thrill-seekers who perform best when
+            extinguishing fires, whether they be real or imagined. They interact
+            with people and the world around them with a sense of dynamic
+            vitality. They make fast assessments of the environment and react
+            skillfully to respond to urgent issues with workable solutions.
+            <br />
+            ESTPs are often the light of the party and have a strong sense of humour.
+            They are fun and active. They assess their audience using their acute
+            observational skills and change direction rapidly to keep encounters
+            engaging. The ESTP loves to keep things fast-paced and humorous rather
+            than emotional or serious, despite the fact that they frequently give
+            off the impression of being very social.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">What does ESTP stand for?</h1>
+        <div class="mt-4">
+            One of the sixteen personality types identified by Isabel Myers and
+            Katharine Briggs is known by the initials ESTP. Extraverted,
+            Sensing, Thinking, and Perceiving are its initials. The personality
+            type ESTP is characterised by extraversion (being energised by other
+            people), sensing (focusing on facts and details rather than ideas
+            and concepts), thinking (making decisions based on logic and
+            reason), and preference for spontaneity and flexibility over planned
+            and organised behaviour (Perceiving). Because of their high-energy,
+            active way of life, ESTPs are also known as "dynamo personalities."
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">ESTP Values and Motivations</h1>
+        <div class="mt-4">
+            Natural athletes, ESTPs can move around their physical environment
+            with ease and have a good sense of coordination. They frequently
+            love putting their skills to the test in risky or even dangerous
+            activities, and they enjoy using this physical aptitude in the
+            search of thrill and adventure.
+            <br />
+            The moment-to-moment focus of the ESTP is action. They interact with
+            their surroundings and find quick solutions to difficulties of a practical
+            nature. ESTPs succeed in urgent situations when they can use their logical
+            thinking to determine the best course of action. The ESTP loves to see
+            immediate, observable results, therefore long-term goals are less intriguing
+            to them.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">How Others See the ESTP</h1>
+        <div class="mt-4">
+            The ESTP's energy is probably what you notice about them right away.
+            They frequently converse, joke around, and flirt with both friends
+            and total strangers. They enjoy having lively interactions with
+            others and making everyone around them laugh with their silly
+            humour. People are frequently kept on their toes since they never
+            know what the ESTP will make fun of next. ESTPs are openly sociable
+            with others, but their interest in people may wane over time. They
+            are more likely to work a room and have a good time with everyone
+            than they are to have a meaningful conversation with any one person.
+            <br />
+            ESTPs feel at ease in their surroundings and are constantly looking for
+            something to do. They are the most naturally coordinated of all the types
+            and are frequently seen participating in sports or other physical activities,
+            particularly ones that include some level of danger. They fit the stereotype
+            of "adrenaline junkies" and can be seen participating in extreme sports
+            like skydiving or racing motorcycles.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">
+            How rare is the ESTP personality type?
+        </h1>
+        <div class="mt-4">
+            ESTPs consist of:
+            <ul>
+                <li>4% of the general population</li>
+                <li>6% of men</li>
+                <li>3% of women</li>
+            </ul>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Famous ESTPs</h1>
+        <div class="mt-4">
+            Famous ESTPs include Evel Knievel, Donald Trump, George W. Bush, Mae
+            West, Eddie Murphy, Miley Cyrus, Bruce Willis, and Winston
+            Churchill.
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">Facts about ESTPs</h1>
+        <div class="mt-4">
+            Interesting facts about the ESTP:
+            <uL>
+                <li>
+                    On personality trait measures, score as Dominant, Flexible,
+                    Demanding, and Sociable
+                </li>
+                <li>
+                    More frequent among patients suffering from chronic pain
+                </li>
+                <li>
+                    One of four types reporting highest levels of assertiveness
+                    in college
+                </li>
+                <li>One of two types with lowest college GPA</li>
+                <li>Among most likely of all types to stay in college</li>
+                <li>
+                    Values at work include autonomy, variety, independence, and
+                    structure
+                </li>
+                <li>Overrepresented among MBA students</li>
+                <li>
+                    Commonly found in careers in marketing, skilled trades,
+                    business, and law enforcement
+                </li>
+            </uL>
+        </div>
+
+        <h1 class="mt-4 text-lg opacity-50">ESTP Hobbies and Interests</h1>
+        <div class="mt-4">
+            Sports and athletic activities of all kinds, especially team sports
+            and dangerous or daring endeavours like racing, boxing, or flying,
+            are popular pastimes for an ESTP.
+        </div>
+    </div>
+</div>
