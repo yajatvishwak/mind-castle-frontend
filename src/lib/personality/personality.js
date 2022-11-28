0bd25aaaf27201f2d0b20aed0788947b68675518
Yajat Vishwak <yajat472001@gmail.com>
2022-11-28 13:54:27 +0530

    ESFJ

--- src/lib/personality/personality.js
+++ src/lib/personality/personality.js
@@ -112,3 +112,252 @@ let ENFJ = {
   visiting museums, listening to music, writing, storytelling, and
   gourmet cooking.`,
 };
+
+let ENFP = {
+  img: "enfp.png",
+  tagline: "THE CHAMPION",
+  desc1: `With a focus on potential and a contagious enthusiasm for novel
+    concepts, individuals, and pursuits, ENFPs are people-centered
+    creators. ENFPs are enthusiastic, vivacious, and love to support
+    others in discovering their creative potential.`,
+  desc2: ` Energetic and expressive communicators, ENFPs frequently use their wit,
+    humour, and command of language to craft engrossing narratives. Being
+    imaginative and creative, ENFPs frequently have a strong creative side.
+    They are attracted to art because it enables them to express creative
+    ideas and develop a richer grasp of the human condition.`,
+  standsfor1: `Each of the four letters that make up the ENFP code stands for a crucial
+    aspect of this personality type. Spending time with others gives ENFPs
+    energy. They also focus on ideas and concepts rather than facts and details,
+    are intuitive rather than analytical, base their decisions on feelings
+    and ideals, and prefer to be flexible and impulsive rather than planned
+    and organised (Perceiving).`,
+  values1: `A common trait of ENFPs is their curiosity about other people and
+    obsession with understanding the true significance of things. They
+    frequently seek emotional intensity and demand true experiences.
+    ENFPs are easily bored by repetition and small details, so they look
+    for situations that provide an escape from the ordinary. For ENFPs,
+    who frequently have a diverse range of interests and friends from
+    various backgrounds, novelty is appealing.`,
+  values2: `ENFPs value individuality and frequently believe that pursuing happiness
+    is the most important thing in life, both for oneself and for others.
+    They value individual autonomy and self-expression highly and desire
+    to follow their inspirations wherever they may go.`,
+
+  see1: `ENFPs enjoy discussing individuals, not just the facts but also what
+    inspires, drives, and goals they have for themselves. They
+    frequently readily communicate their own objectives and are
+    interested in hearing others' as well. The ENFP is unlikely to pass
+    judgement on anyone's dreams and will share even the most
+    fantastical and improbable fantasies with warmth and enthusiasm.
+    Nothing deflates them more quickly than talking about uninteresting
+    facts or harsh reality because they love to consider imaginative
+    possibilities.`,
+  see2: `Due to their tendency to appear disorganised and dispersed, ENFPs frequently
+    give the impression of being out of touch with their immediate environment.
+    As they are more prone to concentrate on interacting with others or on
+    pursuing their own creativity and self-expression, they frequently ignore
+    the nuances. They want to live intensely and colourfully and have little
+    patience for the ordinary. ENFPs frequently have an artistic side and
+    may even look artistic. Many people have evolved a unique and eccentric
+    personal style.`,
+  famous: `Famous ENFPs include Dr. Seuss, Robin Williams, Bill Clinton, Phil
+    Donahue, Mark Twain, Edith Wharton, Will Rogers, Carol Burnett, Joan
+    Baez, Regis Philbin, and Drew Barrymore.`,
+  hobbies: `Writing, making and appreciating art, playing musical instruments,
+  listening to music, taking part in community theater, and reading
+  fiction are all common hobbies for ENFPs.`,
+};
+
+let ENTJ = {
+  img: "entj.png",
+  tagline: "THE COMMANDER",
+  desc1: `Strategic leaders driven to orchestrate change, ENTJs are. They
+    appreciate creating long-term strategies to realise their vision
+    since they are quick to spot inefficiency and envision fresh
+    alternatives. They are typically articulate and perceptive, and they
+    excel in logical reasoning.`,
+  desc2: `ENTJs enjoy providing order to the world around them and are analytical
+    and objective. The ENTJ can identify weaknesses in a system and enjoys
+    the process of finding and implementing a more effective solution. ENTJs
+    are assertive and like to be in charge; they see themselves as managers
+    and leaders who coordinate people and processes to accomplish their objectives.`,
+  standsfor1: `One of the sixteen personality types identified by Isabel Myers and
+    Katharine Briggs is known by the initials ENTJ. Extraverted,
+    iNtuitive, Thinking, and Judging are its initials. The personality
+    type ENTJ describes someone who gets their energy from being around
+    other people (Extraverted), who prefers ideas and concepts to facts
+    and details (iNtuitive), who bases their decisions on logic and
+    reason (Thinking), and who prefers to be organised and planned
+    rather than spontaneous and flexible (Judging). Because of their
+    intrinsic desire to lead others, ENTJs are sometimes referred to as
+    Commander personalities.`,
+  values1: ` ENTJs are frequently highly driven by professional accomplishment
+    and take pleasure in working hard. They have aspirations of being
+    influential and powerful. Making decisions is what they do for a
+    living. They want to be able to make the decision and initiate
+    action.`,
+  values2: ` ENTJs are often direct and determined. They are driven to complete tasks
+    and can occasionally be harsh or critical in the process. They are often
+    outgoing and sociable, yet they occasionally fail to recognise others'
+    subtle emotional cues. They frequently like working with people to achieve
+    a shared objective, but they might not have time to take care of their
+    emotions. They are results-driven and aspire to be effective, knowledgeable,
+    and influential.`,
+
+  see1: `Natural leaders, ENTJs frequently take charge no matter the
+    situation. They frequently have a distinct vision for the future and
+    have an instinctive sense of how to steer individuals and systems in
+    that direction. They frequently approach each scenario with the
+    efficiency analyst mindset and aren't afraid to suggest
+    improvements. For the ENTJ, the success of their plans is a given;
+    it's only a matter of time before they can influence the
+    participants to make everything happen.`,
+  see2: `When meeting new people, ENTJs are frequently sociable and seem to have
+    have a notion of how they will fit into their big picture. They are frequently
+    blunt and come out as cocky or even arrogant; they assess people and
+    circumstances swiftly and struggle to be anything but truthful about
+    what they observe. ENTJs are sensitive to power dynamics and gravitate
+    toward influential roles and individuals. They have the typical ambition
+    and are frequently very committed to their work. ENTJs enjoy their work
+    more than any other personality type; some even claim that it is what
+    they do for fun.`,
+  famous: `Famous ENTJs include David Letterman, Douglas MacArthur, Harrison
+    Ford, Mindy Kaling, and Quentin Tarantino. Others include Margaret
+    Thatcher, Kamala Harris, Napoleon Bonaparte, Carl Sagan, General
+    Norman Schwarzkopf, and David Letterman.`,
+  hobbies: `Taking on leadership roles in neighbourhood organisations, going to
+    social gatherings or sporting events, and participating in
+    competitive sports are all common pastimes among ENTJs. Because
+    ENTJs tend to be so career-focused, they might not have many
+    interests outside of work or they might engage in hobbies that help
+    them advance their jobs.`,
+};
+
+let ENTP = {
+  img: "entp.png",
+  tagline: "THE VISIONARY",
+  desc1: ` ENTPs are inspired innovators who are driven to come up with fresh
+    answers to complex issues. They are intelligent and inquisitive and
+    try to understand the people, institutions, and ideologies around
+    them. Visionaries are unconventional and open-minded people who want
+    to understand, analyze, and influence others.`,
+  desc2: `ENTPs like experimenting with concepts and especially appreciate engaging
+    in conversation. They maintain the upper hand with others by making light
+    of their quirks and habits while also using their quick wit and knowledge
+    of language. Although the ENTP likes to test others, in the end they
+    are usually content to let things go. Although they don't often pass
+    judgment, they might be impatient with those who can't keep up.`,
+  standsfor1: ` One of the sixteen personality types identified by Isabel Myers and
+    Katharine Briggs is known by the initials ENTP. Extraverted,
+    iNtuitive, Thinking, and Perceiving are its initials. ENTP describes
+    a person as being extraverted (energized by time spent with others),
+    intuitive (focuses on ideas and concepts rather than facts and
+    details), thinking (makes decisions using logic and reason), and
+    preferring to be flexible and impromptu rather than planned and
+    organized. Because of their enthusiasm for original, cutting-edge
+    concepts, ENTPs are occasionally referred to as visionary types.`,
+  values1: `ENTPs thrive on a challenge and are frequently motivated by issues
+    that other people think are insurmountable. They may believe that
+    others are too enmeshed in tradition to perceive a new perspective
+    because they are self-assured in their capacity for original
+    thought. The Visionary rarely feels the need for preparation because
+    they rely on their ingenuity to deal with the world around them.
+    They frequently enter a new situation headfirst and have faith in
+    their ability to change as they go.`,
+  values2: `ENTPs excel at reinventing the wheel and frequently object to performing
+    the same activity in the same way twice. They challenge conventions and
+    frequently reject them entirely. The Visionary finds little inspiration
+    in established practises and would much rather experiment with one (or
+    two) novel approaches than follow the norm.`,
+
+  see1: ` The majority of ENTPs are amiable and frequently charming. They
+    frequently strive to appear intelligent and may use their sharp wit
+    and biting humour to wow others. They are interested in their
+    surroundings and desire to understand how things work. The ENTP, on
+    the other hand, believes that the laws of nature were designed to be
+    defied. They enjoy looking for ways to take advantage of the system
+    and identifying weaknesses. This is not to indicate that the
+    Visionary is evil; rather, they simply find regulations to be
+    restrictive and think there may be an undiscovered better, quicker,
+    or more interesting method to accomplish tasks.`,
+  see2: `The ENTP exhibits typical entrepreneurial traits and could be fast to
+    divulge a fresh business concept or creation. They are creative and self-assured,
+    and they are frequently eager to share their many brilliant ideas. The
+    ENTP's zeal for innovation is contagious, and they frequently succeed
+    in persuading others to support their plans. But because they tend to
+    think "big picture," they could have trouble remembering or describing
+    specifics. They tend to be more enthusiastic about exploring an idea
+    than about turning it into reality, and if they don't follow through
+    with all of their many ideas, they might come off as unreliable.`,
+  famous: `Famous ENTPs include "Weird Al" Yankovic, Conan O'Brien, Steve Jobs,
+    Walt Disney, Thomas Edison, Benjamin Franklin, Richard Feynman,
+    Leonardo da Vinci, Niccolo Machiavelli, John Stuart Mill, and Jon
+    Stewart.`,
+  hobbies: `The ENTP enjoys furthering their education, writing, appreciating art,
+    participating in sports, using computers and playing video games,
+    travelling, and attending cultural events.`,
+};
+let ESFJ = {
+  img: "esfj.png",
+  tagline: "THE PROVIDER",
+  desc1: ` ESFJs are energetically committed to their duties, sensitive to the
+    needs of others, and diligent helpers. They are extremely sensitive
+    to their emotional surroundings and perceptive to other people's
+    emotions as well as their own. ESFJs are eager to please and give,
+    and they like an atmosphere of harmony and cooperation.`,
+  desc2: `ESFJs typically put their family and friends first and respect tradition
+    and devotion. They give freely of their time, energy, and feelings. They
+    frequently treat other people's problems as if they were their own and
+    will make an effort to use their strong organising skills to make other
+    people's lives more orderly.`,
+  standsfor1: ` One of the sixteen personality types identified by Isabel Myers and
+    Katharine Briggs is known by the acronym ESFJ. Extraverted, Sensing,
+    Feeling, and Judging are its initials. A person with the personality
+    type ESFJ is extraverted, sensing, feeling, and feeling. They
+    prioritise facts and details above ideas and concepts, focus on
+    facts and details rather than ideas and concepts, and like to be
+    structured and planned rather than spontaneous and flexible
+    (Judging). Because of their concern in providing for others in
+    practical ways, ESFJs are frequently referred to as Provider
+    personalities.`,
+  values1: `ESFJs uphold a high moral standard and expect others to do the same.
+    They frequently have a black-and-white, right-and-wrong perspective
+    on life, and they are typically not afraid to criticise the actions
+    of others. In order to achieve harmony and collaboration, ESFJs
+    believe that everyone should abide by the same set of norms. They
+    perceive a social order in how individuals interact with one
+    another, and they frequently assume roles that allow them to
+    contribute to enforcing that order.`,
+  values2: `ESFJs are typically willing to participate and offer assistance because
+    they have a sense of personal responsibility for other people's needs.
+    They are typically responsible and realistic, placing duty ahead of pleasure,
+    especially when it comes to taking care of others. They frequently have
+    a regular schedule and typically love regularity, which enables them
+    to be well-organized and effective.`,
+
+  see1: `ESFJs are frequently found serving as hosts or hostesses. They
+    frequently volunteer to be the organiser and want to make sure that
+    everyone is taken care of. The ESFJ is a good fit for positions like
+    committee chair, event coordinator, and church volunteer. They often
+    take an active role in their communities and put in a lot of effort
+    to uphold the social order. ESFJs have a keen interest in other
+    people and like learning about the specifics of their lives. Many
+    ESFJs enjoy engaging in gossip; they enjoy telling tales about those
+    around them.`,
+  see2: `ESFJs have a strong moral compass that directs their conduct and sets
+    high standards for others. They frequently hold firm beliefs about how
+    people ought to act and what is right. ESFJs frequently have a keen interest
+    in manners and other social norms. They might have a strict sense of
+    right and wrong, of black and white. Even though they have the best of
+    intentions and merely want everyone to obey the rules so that they can
+    all get along, they can be judgmental of others who they feel are not
+    performing in an appropriate manner. The ESFJ may get overly preoccupied
+    with other people's issues because they want everything to go smoothly
+    for those around them.`,
+  famous: `Famous ESFJs include Ray Kroc, JC Penney, Sally Field, Mary Tyler
+    Moore, Dave Thomas, Sam Walton, Barbara Walters, Ariana Grande,
+    William Howard Taft, and Dave Thomas.`,
+  hobbies: `Volunteering for communal, charitable, or religious groups, holiday
+    celebrations and family traditions, cooking, entertaining, and
+    social sports are among the ESFJs' favourite pastimes.`,
+};
