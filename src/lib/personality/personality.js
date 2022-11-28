d37897e9721eaba40c9fb9e3c9efc7fa9593056d
Yajat Vishwak <yajat472001@gmail.com>
2022-11-28 13:30:37 +0530

    lol

---
+++ src/lib/personality/personality.js
@@ -0,0 +1,114 @@
+let INTP = {
+  img: "intp.png",
+  tagline: "THE ARCHITECT",
+  desc1: `Philosophical innovators, INTPs are enthralled by logical reasoning,
+      systems, and aesthetics. They are theoretically obsessed and look for an
+      overarching principle underlying what they observe. They seek to
+      comprehend life's underlying themes in all of their complexity.`,
+  desc2: ` INTPs are detached, analytical observers who, because they are so
+      engrossed in their thoughts, can appear to be unaware of their
+      surroundings. They spend a lot of time thinking internally, exploring
+      ideas, connecting dots, and trying to understand how things work. The
+      Architect views life as an ongoing investigation into the complexities of
+      the cosmos.`,
+  standsfor1: `The INTP personality type is defined by a person's need for solitude (introverted),
+      focus on ideas and concepts rather than facts and details (iNtuitive), use
+      of logic and reason in decision-making (thinking), and preference for spontaneity
+      and flexibility over planned and organised activities.`,
+  values1: `Although they have a cold demeanour, INTPs are deeply passionate about
+        logic, research, and invention. In order to integrate the concepts they
+        have noticed in their settings, they try to build intricate systems of
+        understanding. They will go to tremendous mental lengths in an effort to
+        come up with original answers to intriguing challenges since their
+        thoughts are sophisticated and busy.`,
+  values2: ` The INTP tends to be unconventional and is less likely to follow the majority
+        in favour of developing their own unique method of doing things. The INTP
+        is wary of traditions and assumptions and is willing to dissect concepts
+        that other people take for granted. INTPs hold little holy and are ruthless
+        in their analysis of ideas and beliefs. Other people who adhere to ideologies
+        that defy reason frequently leave them perplexed.`,
+
+  see1: `INTPs frequently seem eccentric and odd to others because they are so
+        deeply absorbed in their own thinking. The INTP has a very active
+        intellect, and because of their tendency to focus inward, they may
+        overlook little matters like home furnishings or acceptable attire.
+        Instead than engaging in small talk, they can get fervent when
+        discussing subjects like physics, math, computers, or more grandiose
+        cosmic theoretical conundrums. The Architect is frequently more
+        interested in the theory underlying everything than in reality.`,
+  see2: `INTPs often speak clearly and choose their words carefully to convey
+        complicated topics. Even in the most informal of talks, they insist on
+        intellectual rigour, and they are quick to point out errors in logic or
+        ideas. An INTP who is more engaged in examining logic may disregard
+        social graces, and they may insult others by subjecting their deeply
+        held values and beliefs to logical analysis.`,
+  famous: `Famous INTPs include Albert Einstein, Thomas Jefferson, Kristen Stewart,
+        Rene Descartes, Charles Darwin, Marie Curie, Socrates, and Abraham
+        Lincoln.`,
+  hobbies: `Reading, attending art and cultural events, playing chess and other
+        strategy games, writing, taking classes, using computers, camping,
+        trekking, and meditation are all common pastimes for INTPs.`,
+};
+
+let ENFJ = {
+  img: "enfj.png",
+  tagline: "THE TEACHER",
+  desc1: `Idealistic planners, ENFJs are determined to carry out their vision
+    of what is best for humanity. Because of their capacity for
+    recognising potential in others and their charismatic ability to
+    influence others, they frequently serve as catalysts for human
+    growth. They are fervent about the possibilities for people and are
+    values- and vision-driven.`,
+  desc2: ` ENFJs frequently have a lot on their plates and tend to be enthusiastic
+  and driven people. They have a keen awareness of others' needs and are
+  sensitive to human suffering, but they also have a tendency to be upbeat
+  and forward-looking, instinctively sensing opportunities for progress.
+  While the ENFJ is ambitious, it is not self-serving; rather, they feel
+  personally accountable for improving the world.`,
+  standsfor1: `One of the sixteen personality types developed by Isabel Myers and
+  Katharine Briggs is referred to by the acronym ENFJ. Extraverted,
+  iNtuitive, Feeling, and Judging are its initials. Extroverted,
+  iNtuitive, feeling, and extraverted are all characteristics of the
+  ENFJ personality type. They focus on ideas and concepts rather than
+  facts and details, make decisions based on their values and
+  feelings, and prefer to be organised and planned rather than
+  spontaneous and flexible. Because of their desire to foster the
+  growth and development of others, ENFJs are sometimes referred to as
+  teacher personalities.`,
+  values1: `ENFJs are motivated by a strong sense of empathy and altruism. They
+  frequently serve as an emotional barometer for others around them
+  since they have an intuitive awareness of others' emotions. Their
+  compassion, however, is not limited to those close to them; they
+  frequently have a humanitarian outlook and may genuinely care about
+  the woes of the entire human race. They often feel other people's
+  emotions firsthand and are moved to action when they witness
+  suffering.`,
+  values2: `ENFJs value intimate relationships and mutual support and think that
+  working together is the best approach to accomplish goals. They seek
+  approval and are extremely sensitive to both positive and negative criticism.
+  The ENFJ may be frustrated when others are not as sincere in their goals
+  as they are since they want the best not only from themselves but also
+  from others. ENFJs put up a lot of effort to keep their bonds strong
+  and to contribute positively to their families, groups, and communities.`,
+
+  see1: `Natural educators, ENFJs are frequently seen getting others to
+  participate in educational activities. They have a propensity to
+  assume control and direct a group of people toward the experiences
+  and activities that will best serve their learning and development.
+  They have a keen sense of human potential, and they inspire others
+  to develop their strengths further by being warm and charismatic.
+  They are usually active and busy, and they frequently exhibit
+  visible vigour when inspiring others to learn new things.`,
+  see2: `ENFJs are often good communicators with a gift for connecting with others
+  through language. They have a keen sense of interpersonal interactions
+  and like discussing them. They frequently take pleasure in assisting
+  others in resolving personal issues and love imparting their knowledge
+  of people, their emotions, and their motivations. They can become worn
+  out if there is too much negative emotion around because they can be
+  overly sympathetic at times.`,
+  famous: `Oprah Winfrey, Martin Luther King, Jr., Margaret Mead, Maya Angelou,
+  Ralph Nader, Abraham Maslow, and Dr. Phil McGraw are notable ENFJs.`,
+  hobbies: `The ENFJ enjoys planning social gatherings, reading, the arts,
+  visiting museums, listening to music, writing, storytelling, and
+  gourmet cooking.`,
+};
